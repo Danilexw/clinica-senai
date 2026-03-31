40c33a7eefc31b3c19497062d8e8f621e6d675ad
DANILO PEREIRA RIBEIRO <232.977244@universidadesenaicimatec.edu.br>
2026-03-31 14:56:31 -0300

    Conexão com Supabase finalizada com sucesso

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const SUPABASE_URL = 'https://fcctdgqaxlllerhysagu.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; 
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjY3RkZ3FheGxsbGVyaHlzYWd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NzM5NTIsImV4cCI6MjA5MDU0OTk1Mn0.OOKktZcWCRpwo6aS82qHCYynE9HCn0a0zdIrA6JJF5w'; 
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
